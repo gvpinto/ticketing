cd3d3dd7c48f00ca0e05a17a09f4061fb978ee37
Glenn Pinto <gvpinto@gmail.com>
2023-05-09 20:40:01 -0400

    lesson 132 Scaffoling routes

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,1 +1,10 @@
 
+import express from 'express';
+
+const router = express.Router();
+
+router.post('/api/users/signup', (req, res) => {
+    res.send('Hi there signup');
+});
+
+export { router as signupRouter };
