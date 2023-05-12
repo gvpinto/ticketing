0989d6a9964093565e814d0ff95c9cf9675be5bd
Glenn Pinto <gvpinto@gmail.com>
2023-05-12 12:08:54 -0400

    lesson 138: Building an error handling middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there currentuser');
+    res.send('Hi I am the currentuser');
 });
 
 export { router as currentUserRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
@@ -12,7 +12,17 @@ router.post('/api/users/signup', [
         .isLength({ min: 8, max: 20 })
         .withMessage('Password must be between 8 and 20 characters')
 ], (req: Request, res: Response) => {
-    res.send('Hi there signup');
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        throw new Error("Invalid Email or Password");
+    }
+
+    console.log("Creating user");
+    throw new Error("Invalid Database Connection");
+    res.json({});
+
 });
 
 export { router as signupRouter };
