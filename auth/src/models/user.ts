278a1a7886e5b1908e1d2193ea1863c015eedd8d
Glenn Pinto <gvpinto@gmail.com>
2023-06-02 13:22:26 -0400

    Completed section8

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,18 +1,20 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 interface UserAttrs {
     email: string,
     password: string;
 }
 
-interface UserModel extends mongoose.Model<UserDoc> {
-    build(attrs: UserAttrs): any;
-}
-
 interface UserDoc extends mongoose.Document {
     email: string;
     password: string;
 }
+
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -25,16 +27,27 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+
 userSchema.statics.build = (attrs: UserAttrs) => {
     return new User(attrs);
 };
 
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password'));
+        this.set('password', hashed);
+    }
+    next();
+});
+
 const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
-User.build({
-    email: 'gvpinto@gmail.com',
-    password: 'asdfasdfas'
-});
+
+// const user =  User.build({
+//     email: 'gvpinto@gmail.com',
+//     password: 'asdfasdfas'
+// });
+
 
 // const buildUser = (attrs: UserAttrs) => {
 //     return new User(attrs);
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
 
 
 const router = express.Router();
@@ -14,7 +16,7 @@ router.post('/api/users/signup', [
         .trim()
         .isLength({ min: 8, max: 20 })
         .withMessage('Password must be between 8 and 20 characters')
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
 
     const errors = validationResult(req);
 
@@ -22,10 +24,18 @@ router.post('/api/users/signup', [
         throw new RequestValidationError(errors.array());
     }
 
-    console.log("Creating user");
+    const { email, password } = req.body;
 
-    throw new DatabaseConnectionError();
-    res.json({});
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser != null) {
+        throw new BadRequestError('User id in use');
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
 
 });
 
