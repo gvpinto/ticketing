135a2454170fe6a145895a1d4818ce79a2bfb5f2
Piercing Stripes <gvpinto@gmail.com>
2023-06-04 20:39:35 -0400

    Completed 186: Sign in and Sign Up route handlers with authentication and jwt

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,52 @@
-import express from 'express';
+import express, { Response, Request } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import jwt from 'jsonwebtoken';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-    res.send('Hi there signin');
+router.post('/api/users/signin', [
+    body('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+    body('password')
+        .trim()
+        .notEmpty()
+        .withMessage('You must supply a password')
+], validateRequest, async (req: Request, res: Response) => {
+
+    const { email, password } = req.body;
+
+    // Check if user exists
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+        throw new BadRequestError('Invalid Credentials');
+    }
+
+    // Validate Password
+    const passwordMatches = await Password.compare(existingUser.password, password);
+
+    if (!passwordMatches) {
+        throw new BadRequestError('Invalid Credentials');
+    }
+
+    // Create jwt
+    const userJwt = jwt.sign({
+        id: existingUser.id,
+        email: existingUser.email
+    }, process.env.JWT_KEY!);
+
+    // Store it in a session
+    req.session = {
+        jwt: userJwt
+    };
+
+    res.status(200).send(existingUser);
+
 });
 
 export { router as signinRouter };
