ffd7f0aa79217fa2b11f5d36b24003f17132916f
Piercing Stripes <gvpinto@gmail.com>
2023-06-10 12:28:45 -0400

    Completed 200. Test environment setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,35 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signupRouter } from './routes/signup';
-import { signoutRouter } from './routes/signout';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-const app = express();
-app.set('trust proxy', true); // Trust the ingrex proxy
-
-app.use(json());
-app.use(cookieSession({
-    signed: false, // Do not encrypt the cookie
-    secure: true // send cookie only over https connection
-}));
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async () => {
-    throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import mongoose from "mongoose";
+import { app } from './app';
 
 const start = async () => {
 
