0043a45997c1524c980f0c93f896f7ef0cefcdd0
Piercing Stripes <gvpinto@gmail.com>
2023-06-15 22:00:47 -0400

    complete 239. Passing through cookies

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,19 +1,20 @@
 import axios from "axios";
 
+
 const LandingPage = ({ currentUser }) => {
+    // Runs on the Server for the first time just before loading
     console.log(currentUser);
     return (
         <h1>Landing</h1>
     );
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
+    // console.log(req);
     if (typeof window === 'undefined') {
         // On the server
         const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentUser', {
-            headers: {
-                Host: 'ticketing.dev'
-            }
+            headers: req.headers
         });
         return data;
     } else {
@@ -21,7 +22,6 @@ LandingPage.getInitialProps = async () => {
         const { data } = await axios.get('/api/users/currentUser');
         return data;
     }
-    return {};
 };
 
 export default LandingPage;
