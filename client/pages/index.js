5acb8834709789d018dc5b3c1d7074adddc56a35
Piercing Stripes <gvpinto@gmail.com>
2023-06-15 21:56:37 -0400

    completed 238. Specifyig the host

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,27 @@
-const Landing = () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+    console.log(currentUser);
     return (
         <h1>Landing</h1>
     );
 };
 
-export default Landing;
+LandingPage.getInitialProps = async () => {
+    if (typeof window === 'undefined') {
+        // On the server
+        const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentUser', {
+            headers: {
+                Host: 'ticketing.dev'
+            }
+        });
+        return data;
+    } else {
+        // On the browser
+        const { data } = await axios.get('/api/users/currentUser');
+        return data;
+    }
+    return {};
+};
+
+export default LandingPage;
