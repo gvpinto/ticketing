093470d2061dc98e4c6b98053fbf83004b531aa6
Piercing Stripes <gvpinto@gmail.com>
2023-06-23 07:50:24 -0400

    Complete 304. Manual Ack Mode

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,9 +1,22 @@
 import nats from 'node-nats-streaming';
 
+console.clear();
+
 const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222'
 });
 
-Stan.on('connect', () => {
+stan.on('connect', () => {
     console.log('Publisher connected to NATS');
+
+    const data = JSON.stringify({
+        id: 123,
+        type: 'Concert',
+        price: 20
+    });
+
+    stan.publish('ticket:created', data, () => {
+        console.log('Event Published!');
+
+    });
 });
